715b43940a44defc6657159aeb6d5b346676d082
Forth888 <55693171+Forth888@users.noreply.github.com>
2020-07-12 13:10:18 +0100

    Add files via upload

---
+++ Test_GPT2/Test.py
@@ -0,0 +1,11 @@
+import time
+start =time.time()
+x=1+1+1+1+1+1+1+1+1
+end =time.time()
+
+import datetime
+starttime = datetime.datetime.now()
+#long running
+endtime = datetime.datetime.now()
+print (endtime)
+print(end-start)
